e2fc6635ffad86ba0e811096ef6c974b91cc8dbb
Sweets Sweetman <sweetmantech@gmail.com>
2024-01-20 15:18:41 -0300

    I get first working mintAll on sepolia testnet.

--- components/BuyAllButton/BuyAllButton.tsx
+++ components/BuyAllButton/BuyAllButton.tsx
@@ -1,17 +1,23 @@
-import { base } from "viem/chains"
+import { sepolia } from "viem/chains"
+import { zoraCreatorFixedPriceSaleStrategyAddress } from "@zoralabs/protocol-deployments"
 import useCollection from "../../hooks/useCollection"
 import Button from "../Core/Button"
 
 const BuyAllButton = () => {
-  const imTiredOfBeingHypersurveilled = "0x4b130ef4051a35883e3b399b67d13b9bd4224798"
+  const imTiredOfBeingHypersurveilled = "0x7a9d13f9427220fd876af288e9359f8fc411febb"
+  console.log("SWEETS address")
 
-  const { drops, collectAll } = useCollection(imTiredOfBeingHypersurveilled, base.id)
+  // BASE MAINNET "0x4b130ef4051a35883e3b399b67d13b9bd4224798"
+
+  const { drops, collectAll } = useCollection(imTiredOfBeingHypersurveilled, sepolia.id)
   console.log("SWEETS drops", drops)
 
   const handleClick = async () => {
-    const response = await collectAll()
+    const minter = zoraCreatorFixedPriceSaleStrategyAddress[sepolia.id]
+    const response = await collectAll(minter)
     console.log("SWEETS COLLECT ALL RESPONSE", response)
   }
+
   return (
     <Button onClick={handleClick} className="h-[55px] w-[200px]">
       BUY ALL
--- lib/getFormattedDrops.tsx
+++ lib/getFormattedDrops.tsx
@@ -3,7 +3,7 @@ const getFormattedDrops = (raw, chainId) =>
     contractAddress: item.contract.address,
     chainId, // Assuming chainId is the same as in the hook argument
     uri: item.tokenUri,
-    blockNumber: item.mint ? item.mint.blockNumber.toString() : null,
+    blockNumber: item.mint ? item?.mint?.blockNumber?.toString?.() : null,
     type: item.tokenType,
     timestamp: item.timestamp,
     tokenId: item.tokenId,
