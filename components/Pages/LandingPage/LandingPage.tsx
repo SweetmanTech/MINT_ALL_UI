77f3726659cb9ff6e913a81f49580862c165977b
Sweets Sweetman <sweetmantech@gmail.com>
2024-01-19 23:04:37 -0300

    I add connect wallet btn.

--- components/Pages/LandingPage/LandingPage.tsx
+++ components/Pages/LandingPage/LandingPage.tsx
@@ -1,10 +1,13 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit"
 import Layout from "../../Layout"
 import SeoHead from "../../SeoHead"
 import useIsMobile from "../../../hooks/useIsMobile"
 import Button from "../../Core/Button"
+import { useEthersSigner } from "../../../hooks/useEthersSigner"
 
 const LandingPage = () => {
   const isMobile = useIsMobile()
+  const signer = useEthersSigner()
 
   const handleClick = () => {
     console.log("SWEETS HELLO WORLD")
@@ -13,10 +16,13 @@ const LandingPage = () => {
   return (
     <Layout type={isMobile ? "mobile" : "base"}>
       <SeoHead title="MINT ALL" />
-      <div className="flex flex-col justify-center items-center h-[100vh] md:h-full">
-        <Button onClick={handleClick} className="h-[55px] w-[200px]">
-          BUY ALL
-        </Button>
+      <div className="flex flex-col justify-center items-center h-[100vh] md:h-full gap-5">
+        <ConnectButton />
+        {signer && (
+          <Button onClick={handleClick} className="h-[55px] w-[200px]">
+            BUY ALL
+          </Button>
+        )}
       </div>
     </Layout>
   )
