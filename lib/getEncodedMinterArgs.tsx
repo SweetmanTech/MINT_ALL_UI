bbcf078bb15674d8d4b5c0ef96768330b6c4cc51
Sweets Sweetman <sweetmantech@gmail.com>
2024-01-20 22:07:56 -0300

    I remove more files duplicated from onchain-magic package.

--- lib/getEncodedMinterArgs.tsx
+++
@@ -1,5 +0,0 @@
-import { utils } from "ethers"
-
-const getEncodedMinterArgs = (to: string, comment: string) =>
-  utils.defaultAbiCoder.encode(["address", "string"], [to, comment])
-export default getEncodedMinterArgs
